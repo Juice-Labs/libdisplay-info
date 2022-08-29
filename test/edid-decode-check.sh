4e5df436590d3ea5696bd80ce7acfba130a04185
Simon Ser <contact@emersion.fr>
2022-08-29 14:16:36 +0200

    test: rename EDID files with .edid filename extension
    This makes it easier to perform an operation on all EDID files at
    once, e.g. "test/edid-decode-diff.sh test/data/*.edid" to update all
    diffs at once.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- test/edid-decode-check.sh
+++ test/edid-decode-check.sh
@@ -7,11 +7,12 @@ cleanup() {
 trap cleanup EXIT
 
 edid="$1"
+diff="${edid%.edid}.diff"
 "$REF_EDID_DECODE" --skip-hex-dump --check --skip-sha <"$edid" >"$workdir/ref" || [ $? = 254 ]
 "$DI_EDID_DECODE" <"$edid" >"$workdir/di" || [ $? = 254 ]
 
-if [ -f "$edid.diff" ]; then
-	patch "$workdir/ref" "$edid.diff"
+if [ -f "$diff" ]; then
+	patch "$workdir/ref" "$diff"
 fi
 
 diff -u "$workdir/ref" "$workdir/di"
--- test/edid-decode-diff.sh
+++ test/edid-decode-diff.sh
@@ -10,11 +10,12 @@ cleanup() {
 trap cleanup EXIT
 
 for edid in "$@"; do
+	diff="${edid%.edid}.diff"
 	"$REF_EDID_DECODE" --skip-hex-dump --check --skip-sha <"$edid" >"$workdir/ref" || [ $? = 254 ]
 	"$DI_EDID_DECODE" <"$edid" >"$workdir/di" || [ $? = 254 ]
 	if ! diff -u --label ref "$workdir/ref" --label di "$workdir/di" >"$workdir/diff"; then
-		cp "$workdir/diff" "$edid.diff"
+		cp "$workdir/diff" "$diff"
 	else
-		rm -f "$edid.diff"
+		rm -f "$diff"
 	fi
 done
