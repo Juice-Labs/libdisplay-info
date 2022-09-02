186e78c1286afb9cbecc9504ab5683db760e92cc
Simon Ser <contact@emersion.fr>
2022-09-02 09:15:53 +0200

    displayid: validate checksum
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- displayid.c
+++ displayid.c
@@ -12,6 +12,19 @@
  */
 #define DISPLAYID_MAX_SIZE 256
 
+static bool
+validate_checksum(const uint8_t *data, size_t size)
+{
+	uint8_t sum = 0;
+	size_t i;
+
+	for (i = 0; i < size; i++) {
+		sum += data[i];
+	}
+
+	return sum == 0;
+}
+
 bool
 _di_displayid_parse(struct di_displayid *displayid, const uint8_t *data,
 		    size_t size, struct di_logger *logger)
@@ -33,6 +46,11 @@ _di_displayid_parse(struct di_displayid *displayid, const uint8_t *data,
 
 	section_size = (size_t) data[0x01] + DISPLAYID_MIN_SIZE;
 	if (section_size > DISPLAYID_MAX_SIZE || section_size > size) {
+		errno = EINVAL;
+		return false;
+	}
+
+	if (!validate_checksum(data, section_size)) {
 		errno = EINVAL;
 		return false;
 	}
