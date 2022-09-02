106947ac1ff0dea187b59b3c08c2634ee55afc90
Simon Ser <contact@emersion.fr>
2022-09-02 08:47:38 +0200

    displayid: parse section size
    The section size might be smaller than the provided buffer size,
    because the buffer might have padding at the end.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- displayid.c
+++ displayid.c
@@ -3,10 +3,21 @@
 #include "bits.h"
 #include "displayid.h"
 
+/**
+ * The size of the mandatory fields in a DisplayID section.
+ */
+#define DISPLAYID_MIN_SIZE 5
+/**
+ * The maximum size of a DisplayID section.
+ */
+#define DISPLAYID_MAX_SIZE 256
+
 bool
 _di_displayid_parse(struct di_displayid *displayid, const uint8_t *data,
 		    size_t size, struct di_logger *logger)
 {
+	size_t section_size;
+
 	if (size < 5) {
 		errno = EINVAL;
 		return false;
@@ -16,6 +27,12 @@ _di_displayid_parse(struct di_displayid *displayid, const uint8_t *data,
 	displayid->revision = get_bit_range(data[0x00], 3, 0);
 	if (displayid->version == 0 || displayid->version > 1) {
 		errno = ENOTSUP;
+		return false;
+	}
+
+	section_size = (size_t) data[0x01] + DISPLAYID_MIN_SIZE;
+	if (section_size > DISPLAYID_MAX_SIZE || section_size > size) {
+		errno = EINVAL;
 		return false;
 	}
 
