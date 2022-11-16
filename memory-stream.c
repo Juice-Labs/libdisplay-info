6de6d6a62acbf63c9ee91e3dc5dbdcc6259259bf
Joshua Ashton <joshua@froggi.es>
2022-11-16 14:09:13 +0000

    memory-stream: Implement for Windows

--- memory-stream.c
+++ memory-stream.c
@@ -2,6 +2,8 @@
 
 #include "memory-stream.h"
 
+#ifndef _WIN32
+
 bool
 memory_stream_open(struct memory_stream *m)
 {
@@ -28,3 +30,47 @@ memory_stream_close(struct memory_stream *m)
 
 	return str;
 }
+
+#else
+
+bool
+memory_stream_open(struct memory_stream *m)
+{
+	*m = (struct memory_stream){ 0 };
+	m->fp = tmpfile();
+
+	return m->fp != NULL;
+}
+
+char *
+memory_stream_close(struct memory_stream *m)
+{
+	size_t size;
+	char* str;
+	FILE *fp;
+
+	fp = m->fp;
+	*m = (struct memory_stream){ 0 };
+
+	if (fp == NULL) {
+		return NULL;
+	}
+
+	_fseeki64(fp, 0ll, SEEK_END);
+	size = (size_t)_ftelli64(fp);
+	_fseeki64(fp, 0ll, SEEK_SET);
+
+	str  = malloc(size + 1);
+	if (str == NULL) {
+		fclose(fp);
+		return NULL;
+	}
+
+	fread(str, 1, size, fp);
+	str[size] = '\0';
+	fclose(fp);
+
+	return str;
+}
+
+#endif
